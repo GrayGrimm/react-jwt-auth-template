3fea63b929511c6f5d7168d4e63572a74df1cdc5
Shaina G <shainagtherapy@gmail.com>
2025-08-18 10:19:42 -0700

    Syntax corrections

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
     return (
         <nav>
             <ul>
-                <li>< Link to='/sign-up'>Sign Up</link></li>
+                <li><Link to='/sign-up'>Sign Up</Link></li>
             </ul>
         </nav>
     );
