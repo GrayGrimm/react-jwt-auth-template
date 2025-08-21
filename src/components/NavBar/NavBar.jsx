b1e3df1dc6001b0497aec1e077dd4e8f66eb3fb8
guyera <thegraygrimm@graymansurvivaltech.com>
2025-08-21 15:06:17 -0400

    Create sign out functionality: Complete

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,19 +8,20 @@ import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
-  // Pass the UserContext object to the useContext hook to access:
-  // - The user state (which we use here).
-  // - The setUser function to update the user state (which we aren't using).
-  //
-  // Destructure the object returned by the useContext hook for easy access
-  // to the data we added to the context with familiar names.
-  const { user } = useContext(UserContext);
+  
+  const { user, setUser } = useContext(UserContext);
+
+  const handleSignOut = async () => {
+    localStorage.removeItem('token')
+    setUser(null);    
+  }
 
   return (
     <nav>
       {user ? (
         <ul>
           <li>Welcome, {user.username}</li>
+          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
         </ul>
       ) : (
         <ul>
