b9310c4b109e77007fb04ad635ee4c2093bd149c
Shaina G <shainagtherapy@gmail.com>
2025-08-21 11:55:35 -0700

    catching up

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -3,6 +3,16 @@ import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
+
+const getUserFromToken = () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) return null;
+
+  return JSON.parse(atob(token.split('.')[1])).payload;
+};
+
+
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
